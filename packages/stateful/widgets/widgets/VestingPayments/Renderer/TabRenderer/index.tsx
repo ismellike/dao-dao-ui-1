31119cf5a6253d0497ba8938d96ebefe72338968
Noah Saso <noahsaso@gmail.com>
2024-11-05 10:05:20 -0500

    fixed vesting payments erroring if DAO has account on non-indexed chain 2

--- packages/stateful/widgets/widgets/VestingPayments/Renderer/TabRenderer/index.tsx
+++ packages/stateful/widgets/widgets/VestingPayments/Renderer/TabRenderer/index.tsx
@@ -82,7 +82,9 @@ export const TabRenderer = ({
     ],
     combine: makeCombineQueryResultsIntoLoadingDataWithError({
       firstLoad: 'one',
-      // Ignore errors from any of the queries unless all of them error.
+      // Ignore errors from any of the queries unless all of them error. This is
+      // important in case the DAO has an account on a non-indexed chain, which
+      // causes the last query to error.
       errorIf: 'all',
     }),
   })
